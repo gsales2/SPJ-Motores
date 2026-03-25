bd6248548378cb06ae31dab070e8674783f5b2e2
Gabriel Sales <gabrielsales2002@gmail.com>
2026-03-25 14:15:16 -0300

    finaizado o hero da pagina para mobile e desktop

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,17 @@
+import React from 'react'
 import './Hero.css'
 
 export function Hero() {
     return (
-        <section className='hero'>
+        <section className='hero' id='home'>
             <div className="hero-content">
                 <h1>Motores & Bombas <span>com quem entende</span></h1>
                 <p>Manutenção especializada com 10 anos de tradição em Jacareí</p>
-                <a href="#contato" className='btn-primary'>Fazer orçamento</a>
+                <div className="hero-btns">
+                    <a href="#orçamento" className="btn-hero">
+                        Solicitar Orçamento
+                    </a>
+                </div>
             </div>
         </section>
     )
